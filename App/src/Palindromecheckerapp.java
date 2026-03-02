61ce86786a9de85aaf0360a7fe24da172cdddce4
anandharuban11 <anandharuban111@gmail.com>
2026-03-02 15:50:19 +0530

    UC6 is done

--- App/src/Palindromecheckerapp.java
+++ App/src/Palindromecheckerapp.java
@@ -1,4 +1,27 @@
 package PACKAGE_NAME;
-
+import java.util.*;
 public class Palindromecheckerapp {
+    public static boolean isPalindrome(String str) {
+        Queue<Character> queue = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
+        str = str.toLowerCase();
+
+        for (char ch : str.toCharArray()) {
+            queue.add(ch);
+            stack.push(ch);
+        }
+
+        while (!queue.isEmpty()) {
+            if (!queue.poll().equals(stack.pop())) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+        System.out.println("\"" + input + "\" is " + (isPalindrome(input) ? "" : "not ") + "a palindrome.");
+        sc.close();
+    }
 }
