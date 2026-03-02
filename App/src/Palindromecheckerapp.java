530b9007792bef3cb5606401cbce8b34efd7aa34
anandharuban11 <anandharuban111@gmail.com>
2026-03-02 15:39:01 +0530

    UC5 is done

--- App/src/Palindromecheckerapp.java
+++ App/src/Palindromecheckerapp.java
@@ -1,4 +1,28 @@
 package PACKAGE_NAME;
+import java.util.Stack;
 
 public class Palindromecheckerapp {
+
+    public static void main(String[] args) {
+        String input = "madam";
+        Stack<Character> stack = new Stack<>();
+
+        // Push all characters into stack
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
+        }
+
+        boolean isPalindrome = true;
+
+        // Pop characters and compare with original string
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
+    }
 }
