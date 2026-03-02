31411fece20166943bbbb4fc4715fd35a11becd3
anandharuban11 <anandharuban111@gmail.com>
2026-03-02 15:27:47 +0530

    UC4 is done

--- App/src/Palindromecheckerapp.java
+++ App/src/Palindromecheckerapp.java
@@ -1,4 +1,25 @@
 package PACKAGE_NAME;
 
 public class Palindromecheckerapp {
+
+    public static void main(String[] args) {
+        String input = "madam";
+        char[] chars = input.toCharArray();
+        boolean isPalindrome = true;
+
+        int start = 0;
+        int end = chars.length - 1;
+
+        while (start < end) {
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
+    }
 }
