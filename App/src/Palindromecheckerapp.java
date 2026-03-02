f03e5de56d80e7202af2d852714dd79f143ac319
anandharuban11 <anandharuban111@gmail.com>
2026-03-02 14:28:26 +0530

    UC1:Application Entry & Welcome Message

--- App/src/Palindromecheckerapp.java
+++ App/src/Palindromecheckerapp.java
@@ -1,4 +1,5 @@
-package PACKAGE_NAME;
-
 public class Palindromecheckerapp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Palindrome Checker App Management system ");
+    }
 }
